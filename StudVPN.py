e801c6034b7387e609531622c51273f98b80f941
1 <glebstudent@MacBook-Air-Gleb.local>
2025-02-03 16:29:56 +0300

    Добавил разделения на срок оплаты кнопки

--- StudVPN.py
+++ StudVPN.py
@@ -3,6 +3,7 @@ import uuid
 import json
 import paramiko
 from telebot import types
+from database_utils import create_table, add_user, get_user_by_username, get_user_by_uuid, update_user_subscription, generate_referral_link, get_user_by_referral_uuid
 
 # Настройки вашего бота
 TELEGRAM_TOKEN = '7948987856:AAERs2G3QxGXKl2J8erLzrJpy5bDH39eHUg'
@@ -13,7 +14,6 @@ SERVER_PASSWORD = 'HX6qP0WlYzox'
 CONFIG_FILE_PATH = '/usr/local/etc/xray/config.json'
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
-clients = {}
 
 
 def generate_vless_link(user_id):
@@ -42,15 +42,6 @@ def update_server_config(new_uuid,user_id):
         ssh.connect(SERVER_IP, username=SERVER_USERNAME, password=SERVER_PASSWORD)
         sftp = ssh.open_sftp()
 
-        fl=0
-        cur_uuid=""
-        if user_id in clients:
-            fl=1
-            cur_uuid=clients[user_id]
-
-        if fl==0:
-            clients[user_id]=new_uuid
-
 
         with sftp.open(CONFIG_FILE_PATH, 'r') as config_file:
             config = json.load(config_file)
@@ -59,20 +50,10 @@ def update_server_config(new_uuid,user_id):
         if 'inbounds' in config:
             for inbound in config['inbounds']:
                 if 'settings' in inbound and 'clients' in inbound['settings']:
-                    if fl==1:
                         for client in inbound['settings']['clients']:
                             if inbound['settings']['clients'][client]['id']==cur_uuid:
                                 inbound['settings']['clients'][client]['id']=new_uuid
-                                clients[user_id]=new_uuid
                                 break
-
-                    else:
-                        new_client = {
-                            'id': new_uuid
-                        }
-                        inbound['settings']['clients'].append(new_client)
-
-
 
         # Сохранение обновленной конфигурации
         with sftp.open(CONFIG_FILE_PATH, 'w') as config_file:
@@ -91,16 +72,15 @@ def update_server_config(new_uuid,user_id):
 @bot.message_handler(commands=['start'])
 def start(message):
     welcome_message = (
+        "Рады приветствовать тебя в нашем ВПН \n\n"
         "🚀 Безопасный и быстрый VPN у вас под рукой! 🔒\n\n"
-        "Забудьте о блокировках и ограничениях. Наш бот предоставит вам надежный VPN-сервис для безопасного и анонимного доступа в интернет.\n\n"
-        "➡️ Начните использовать VPN прямо сейчас, нажав на кнопку ниже!\n\n"
-        "🛡️ Анонимность | ⚡ Скорость | 🌎 Доступность\n\n"
+        "Забудьте о блокировках и не надежном соединении.\n\n"
+        "С нашим ботом у вас будет: \n"
+        "*   Самая высокая скорость\n"
         "*   Конфиденциальность ваших данных\n"
-        "*   Обход блокировок и ограничений\n"
+        "*   Удобный и понятный интерфейс\n"
         "*   Защита в публичных Wi-Fi сетях"
     )
-
-    bot.send_message(message.chat.id, welcome_message)
     user_id = message.from_user.id  # Получаем user_id
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add("Купить VPN", "Мой VPN", "Реферальная программа", "Поддержка", "О нас/FAQ")
@@ -112,17 +92,26 @@ def start(message):
 @bot.message_handler(func=lambda message: message.text == "Купить VPN")
 def buy_vpn(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.add("Iphone", "Android", "MacBook", "Windows" "Главное меню")
+    markup.add("iPhone", "Android", "MacBook", "Windows" "Главное меню")
     bot.send_message(message.chat.id, "Выберите устройство, для которого хотите купить ВПН:", reply_markup=markup)
 
 
 
-@bot.message_handler(func=lambda message: message.text in ["iPhone", "Android", "Mac", "Windows"])
+@bot.message_handler(func=lambda message: message.text in ["iPhone", "Android", "MacBook", "Windows"])
 def choose_subscription_duration(message):
     device = message.text
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add("1 месяц - 99₽", "3 месяца - 259₽", "6 месяцев - 499₽", "12 месяцев - 999₽", "Назад")
     bot.send_message(message.chat.id, f"Вы выбрали {device}. Выберите срок подписки:", reply_markup=markup)
+
+
+
+@bot.message_handler(func=lambda message: message.text in ["1 месяц - 99₽", "3 месяца - 259₽", "6 месяцев - 499₽", "12 месяцев - 999₽"])
+def choose_subscription_duration(message):
+    user_id = message.from_user.id  #
+    vless_link = generate_vless_link(user_id)
+    bot.send_message(message.chat.id, f"Ваша VLESS ссылка:")
+    bot.send_message(message.chat.id, vless_link)
 
 
 #Обработчик команды "Назад"
@@ -177,11 +166,6 @@ def support(message):
     bot.send_message(message.chat.id, "Вам ответит первый освободившийся модератор")
     bot.send_message(message.chat.id, "@gblev")
 
-#О нас
-@bot.message_handler(func=lambda message: message.text == "О нас/FAQ")
-def about_faq(message):
-    faq = ( "О нас:\n" "Мы предоставляем надежные VPN-услуги для защиты вашей конфиденциальности.\n" )
-    bot.send_message(message.chat.id, faq)
 
 
 
